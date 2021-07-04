3f3038eb65e566df801b6d63723cab55deca3cc2
NirajanBekoju <nirajan.bekoju@gmail.com>
2021-07-04 17:29:44 +0545

    PostgreSQL used

--- esgb/settings.py
+++ esgb/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'esgb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'esgb',
+        'USER': 'admin',
+        'PASSWORD': 'as*1Js0i7',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
