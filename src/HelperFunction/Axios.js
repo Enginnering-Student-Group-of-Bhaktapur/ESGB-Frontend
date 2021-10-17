ada927c740d892bba7f2fc2c273f2808f8dcf364
Bishad Koju <kojubishad@gmail.com>
2021-10-17 20:38:35 +0545

    deployment test 6

--- src/HelperFunction/Axios.js
+++ src/HelperFunction/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const baseURL = "http://127.0.0.1:8000/api";
+// http://localhost:8000 is added automatically due to proxy key in package.json
+const baseURL = "/api";
 
 const axiosInstance = axios.create({
     baseURL : baseURL,  
