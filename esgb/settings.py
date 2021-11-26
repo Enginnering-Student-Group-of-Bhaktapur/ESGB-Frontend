e3b5656d5d7ce01e2c3d7518e84f8c53cc204507
NirajanBekoju <nirajan.bekoju@gmail.com>
2021-11-26 11:41:14 +0545

    Event Data Added

--- esgb/settings.py
+++ esgb/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-y2qo!l5w*$45_=l9cv^xjub@h16wv0#si#%21akm!@39ggc878
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
--- scripts/load.py
+++ scripts/load.py
@@ -1,26 +1,39 @@
 import csv
 import os
 from aboutus.models import Member
+from events.models import Event
 
 def run():
-    file = open('/media/nirajan/1C067838067814CC1/data/member.csv')
-    read_file = csv.reader(file)
+    # Uploading the member data
+    member_file = open('/media/nirajan/1C067838067814CC1/data/member.csv')
+    read_member_file = csv.reader(member_file)
 
     Member.objects.all().delete()
 
     count = 1
 
-    for record in read_file:
+    for record in read_member_file:
         if count == 1:
             pass
         else:
-            batch = record[0]
-            faculty = record[1]
-            name = record[2]
-            email = record[3]
-            current_address = record[4]
-            permanent_address = record[5]
-            contact_1 = record[6]
-            contact_2 = record[7]
+            batch, faculty, name, email, current_address, permanent_address, contact_1, contact_2 = record
             Member.objects.create(name = name, batch = batch, faculty = faculty, email = email, address = current_address, phone_number_1 = contact_1, phone_number_2 = contact_2, position = "Member")            
         count += 1
+
+    # Uploading the member data
+    event_file = open('/media/nirajan/1C067838067814CC1/data/event_data.csv')
+    read_event_file = csv.reader(event_file)
+
+    Event.objects.all().delete()
+
+    count = 1
+
+    for record in read_event_file:
+        if count == 1:
+            pass
+        else:
+            date, name, venue, category, description = record
+            Event.objects.create(title = name, slug = name, date_of_event = date, venue = venue, category = category, description = description)
+        count += 1
+    
+    
